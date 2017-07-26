2435b74ba1322d490d1c6742c8a7b43132fc6fc3
Taylor Cramer <cramertj@google.com>
2017-07-26 12:07:55 -0700

    Writes to servicecontrol GRPC

--- src/write_test_plugin.cc
+++ src/write_test_plugin.cc
@@ -4,11 +4,13 @@ extern "C" {
 #include "collectd.h"
 #include "common.h"
 #include "plugin.h"
+#include "string.h"
 
 #include "daemon/utils_cache.h"
 }
 
 #include <memory>
+#include <uuid/uuid.h> // TODO: add uuid-dev to deps in configure
 #include <vector>
 
 #include <google/protobuf/repeated_field.h>
@@ -20,10 +22,14 @@ extern "C" {
 #include <grpc++/create_channel.h>
 #include <grpc++/security/credentials.h>
 
-#include "google/api/servicecontrol/v1/service_controller.pb.h"
+#include "google/api/servicecontrol/v1/service_controller.grpc.pb.h"
 
 using google::api::servicecontrol::v1::MetricValue;
 using google::api::servicecontrol::v1::MetricValueSet;
+using google::api::servicecontrol::v1::Operation;
+using google::api::servicecontrol::v1::ReportRequest;
+using google::api::servicecontrol::v1::ReportResponse;
+using google::api::servicecontrol::v1::ServiceController;
 
 using google::protobuf::Map;
 using google::protobuf::RepeatedPtrField;
@@ -33,26 +39,148 @@ using google::protobuf::util::TimeUtil;
 
 static const char this_plugin_name[] = "write_test_plugin";
 
-static Map<string, string> metadata_to_lables(meta_data_t *meta) {
-  // Items to clean up on exit
-  char **toc = nullptr;
-  int toc_size = 0;
-  int result = -1;
+class ServiceControllerClient {
+  public:
+    ServiceControllerClient(std::shared_ptr<grpc::Channel> channel)
+      : stub_(ServiceController::NewStub(channel)) {}
 
-  if (meta != nullptr) {
-    toc_size = meta_data_toc(meta, &toc);
-    if (toc_size < 0) {
+  int Report(ReportRequest request) {
+    ReportResponse response;
+    grpc::ClientContext context;
+    grpc::Status status = stub_->Report(&context, request, &response);
+
+    if (status.ok()) {
+      return 0;
+    } else {
+      ERROR("%s: ServiceControllerClient::Report failed with error code %d: %s",
+            this_plugin_name, status.error_code(), status.error_message().c_str());
+      return -1;
     }
   }
 
-  leave:
-    ERROR("%s: meta_data_to_labels: error reading metadata table of contents.");
+  private:
+    std::unique_ptr<ServiceController::Stub> stub_;
+};
+
+bool is_prefix_of(const char* str1, const char* str2) {
+    return (strncmp(str1, str2, strlen(str1)) == 0);
 }
 
-static void raw_values_to_metric_value_sets(
+static int metadata_value_as_string(meta_data_t *meta, const char* key, string *str_out) {
+  switch(meta_data_type(meta, key)) {
+  case MD_TYPE_STRING:
+    char *md_string;
+    if (meta_data_get_string(meta, key, &md_string) != 0 || md_string == nullptr) {
+      return -1;
+    }
+    *str_out = string(md_string);
+    free(md_string);
+    return 0;
+  case MD_TYPE_SIGNED_INT:
+    int64_t md_int;
+    if (meta_data_get_signed_int(meta, key, &md_int) != 0) {
+      return -1;
+    }
+    *str_out = std::to_string(md_int);
+    return 0;
+  case MD_TYPE_UNSIGNED_INT:
+    uint64_t md_uint;
+    if (meta_data_get_unsigned_int(meta, key, &md_uint) != 0) {
+      return -1;
+    }
+    *str_out = std::to_string(md_uint);
+    return 0;
+  case MD_TYPE_DOUBLE:
+    double md_double;
+    if (meta_data_get_double(meta, key, &md_double) != 0) {
+      return -1;
+    }
+    *str_out = std::to_string(md_double);
+  case MD_TYPE_BOOLEAN:
+    bool md_bool;
+    if (meta_data_get_boolean(meta, key, &md_bool) != 0) {
+      return -1;
+    }
+    *str_out = std::to_string(md_double);
+  default:
+    return -1;
+  }
+}
+
+// Returns 0 on success, -1 on failure, -2 on non-Chemist metrics
+static int name_and_labels_from_metadata(
+  meta_data_t *meta,
+  string *name_out,
+  Map<string, string> *labels_out
+) { /* {{{ */
+  if (meta == nullptr) {
+    return -2;
+  }
+
+  char **meta_data_keys = nullptr;
+  int meta_data_keys_len = meta_data_toc(meta, &meta_data_keys);
+  if (meta_data_keys_len < 0) {
+    ERROR("%s: names_and_labels_from_metadata: "
+          "error reading metadata table of contents.", this_plugin_name);
+    return -1;
+  }
+
+  bool found_name = false;
+  for (int i = 0; i < meta_data_keys_len; i++) {
+    char *key = meta_data_keys[i];
+
+    if (strcmp("stackdriver_metric_type", key) == 0) {
+      found_name = true;
+
+      if (meta_data_type(meta, key) != MD_TYPE_STRING) {
+        strarray_free(meta_data_keys, meta_data_keys_len);
+        ERROR("%s: names_and_labels_from_metadata: "
+              "metric name of non-string type", this_plugin_name);
+        return -1;
+      }
+
+      char *metric_name;
+      if (meta_data_get_string(meta, key, &metric_name) != 0 || metric_name == nullptr) {
+        strarray_free(meta_data_keys, meta_data_keys_len);
+        ERROR("%s: names_and_labels_from_metadata: "
+              "error getting name (string) from metadata", this_plugin_name);
+        return -1;
+      }
+      *name_out = string(metric_name);
+      free(metric_name);
+    } else if (is_prefix_of("label:", key)) {
+      char *label_name = key + strlen("label:");
+      string label_value;
+      if (metadata_value_as_string(meta, key, &label_value) < 0) {
+        strarray_free(meta_data_keys, meta_data_keys_len);
+        ERROR("%s: names_and_labels_from_metadata: "
+              "error getting metadata with key \"%s\" as string",
+              this_plugin_name, key);
+        return -1;
+      }
+      (*labels_out)[string(label_name)] = label_value;
+    }
+  }
+
+  strarray_free(meta_data_keys, meta_data_keys_len);
+
+  if (found_name) {
+    return 0;
+  } else {
+    return -2;
+  }
+} /* }}} name_and_labels_from_metadata */
+
+static int raw_values_to_metric_value_sets(
     const data_set_t *cd_datasource,
     const value_list_t *cd_values,
     RepeatedPtrField<MetricValueSet> *value_sets_out) { /* {{{ */
+
+  string metric_name;
+  Map<string, string> labels_map;
+  if (name_and_labels_from_metadata(cd_values->meta, &metric_name, &labels_map) != 0) {
+    return -1;
+  }
 
   int64_t start_ns = CDTIME_T_TO_NS(cd_values->time);
   int64_t duration_ns = CDTIME_T_TO_NS(cd_values->interval);
@@ -62,20 +190,20 @@ static void raw_values_to_metric_value_sets(
       ERROR(": overflow of start_ns + duration_ns");
   }
 
-
   unsigned int i;
   for (i = 0; i < cd_datasource->ds_num; i++) {
     data_source_t *src = &cd_datasource->ds[i];
     value_t value = cd_values->values[i];
 
     MetricValueSet *new_value_set = value_sets_out->Add();
-    new_value_set->set_metric_name(&src->name[0]);
+    new_value_set->set_metric_name(metric_name);
 
     RepeatedPtrField<MetricValue> *new_values = new_value_set->mutable_metric_values();
     MetricValue *new_value = new_values->Add();
-    Map<string, string> *labels = new_value->mutable_labels();
     *new_value->mutable_start_time() = start_time;
     *new_value->mutable_end_time() = end_time;
+
+    *new_value->mutable_labels() = Map<string, string>(labels_map);
 
     // TODO write value as bool based on inception config
     switch (src->type) {
@@ -99,6 +227,8 @@ static void raw_values_to_metric_value_sets(
         continue;
     }
   }
+
+  return 0;
 } /* }}} raw_values_to_metric_value_sets */
 
 
@@ -158,11 +288,40 @@ static int wtest_write(const data_set_t *ds,
                        user_data_t *user_data) { /* {{{ */
   DEBUG("Logging shtuff from wtest_write");
   assert(ds->ds_num > 0);
-  wtest_context_t *ctx = (wtest_context_t *)user_data->data;
 
-  auto channel = grpc::CreateChannel(
-                    "localhost:50051",
-                    grpc::InsecureChannelCredentials());
+  // TODO: use the context to store a reusable client channel/context
+  wtest_context_t *ctx = (wtest_context_t *)user_data->data;
+  (void)ctx;
+
+  Operation operation;
+  if (raw_values_to_metric_value_sets(
+        ds, vl, operation.mutable_metric_value_sets()) != 0) {
+    // Either the current value wasn't flagged for reporting to
+    // ServiceController, or we failed to process it correctly.
+    //
+    // If we failed to process it correctly, an error has already
+    // been reported, so we ignore the result.
+    return 0;
+  }
+
+  uuid_t uuid;
+  uuid_generate(uuid);
+  char uuid_chars [37]; // UUIDs are 36 characters + trailing '\0'
+  uuid_unparse(uuid, &uuid_chars[0]);
+  string uuid_string(uuid_chars);
+  operation.set_operation_id(uuid_string);
+
+  // TODO: set global operation labels
+
+  ReportRequest request;
+  request.set_service_name("dummy_service_name");
+  *request.add_operations() = operation;
+
+  auto creds = grpc::GoogleComputeEngineCredentials();
+  auto channel = grpc::CreateChannel("localhost:50051", creds);
+  
+  ServiceControllerClient client(channel);
+  client.Report(request);
 
   return 0;
 } /* }}} wtest_write */
